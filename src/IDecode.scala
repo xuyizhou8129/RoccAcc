124f10212cdce5e3d0b3cef5e88e3d68e00eac10
Xuyi Zhou <zhouxuyi20060401@gmail.com>
2025-07-02 00:45:56 -0500

    Creating a source code folder

---
+++ src/IDecode.scala
@@ -0,0 +1,59 @@
+package vcoderocc
+
+import chisel3._
+import chisel3.util._
+import freechips.rocketchip.config.Parameters
+import freechips.rocketchip.tile.HasCoreParameters
+import Instructions._
+import vcoderocc.constants._
+
+/** Trait holding an abstract (non-instantiated) mapping between the instruction
+  * bit pattern and its control signals.
+  */
+trait DecodeConstants extends HasCoreParameters { // TODO: Not sure if extends needed
+  /** Array of pairs (table) mapping between instruction bit patterns and control
+    * signals. */
+  val decode_table: Array[(BitPat, List[BitPat])]
+}
+
+/* Control signals in the processor.
+  * These are set during decoding.
+  */
+class CtrlSigs extends Bundle { // TODO: Rename to BinOpCtrlSigs?
+  /* All control signals used in this coprocessor
+    See rocket-chip's rocket/IDecode.scala#IntCtrlSigs#default */
+    val legal = Bool() // Example control signal.
+  /* List of default control signal values
+    @return List of default control signal values. */
+    
+    def default_decode_ctrl_sigs: List[BitPat] =
+    List(N)
+
+  /** Decodes an instruction to its control signals.
+    * @param inst The instruction bit pattern to be decoded.
+    * @param table Table of instruction bit patterns mapping to list of control
+    * signal values.
+    * @return Sequence of control signal values for the provided instruction.
+    */
+  def decode(inst: UInt, decode_table: Iterable[(BitPat, List[BitPat])]) = {
+    val decoder = freechips.rocketchip.rocket.DecodeLogic(inst, default_decode_ctrl_sigs, decode_table)
+    /* Make sequence ordered how signals are ordered.
+     * See rocket-chip's rocket/IDecode.scala#IntCtrlSigs#decode#sigs */
+    val ctrl_sigs = Seq(legal)
+    /* Decoder is a minimized truth-table. We partially apply the map here,
+     * which allows us to apply an instruction to get its control signals back.
+     * We then zip that with the sequence of names for the control signals. */
+    ctrl_sigs zip decoder map{case(s,d) => s := d}
+    this
+  }
+}
+
+/** Class holding a table that implements the DecodeConstants table that mapping
+  * a binary operation's instruction bit pattern to control signals.
+  * @param p Implicit parameter of key-value pairs that can globally alter the
+  * parameters of the design during elaboration.
+  */
+class BinOpDecode(implicit val p: Parameters) extends DecodeConstants {
+  val decode_table: Array[(BitPat, List[BitPat])] = Array(
+    PLUS_INT-> List())
+}
