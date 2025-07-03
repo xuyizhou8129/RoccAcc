891b2fc24eb53f646ffdd49912edf0054a2ac5e3
Xuyi Zhou <zhouxuyi20060401@gmail.com>
2025-07-03 16:20:01 -0500

    Adding Fetching place holder

---
+++ src/DFetch.scala
@@ -0,0 +1,32 @@
+package vcoderocc
+
+import chisel3._
+import chisel3.util._
+import freechips.rocketchip.config.Parameters
+import freechips.rocketchip.tile.CoreModule
+import freechips.rocketchip.rocket.constants.MemoryOpConstants
+
+/* TODO: Investigate if we should use RoCCCoreIO.mem (DCacheFetcher) or
+ * LazyRoCC.tlNode (DMemFetcher).
+ * RoCCCoreIO.mem connects to the local main processor's L1 D$ to perform
+ * operations.
+ * LazyRoCC.tlNode connects to the L1-L2 crossbar connecting this tile to the
+ * larger system. */
+
+/** Module connecting VCode accelerator to main processor's non-blocking L1 data
+  * cache.
+  * @param p Implicit parameter passed by build system of top-level design parameters.
+  */
+class DCacheFetcher(implicit p: Parameters) extends CoreModule()(p) {
+  /* For now, we only support "raw" loading and storing.
+   * Only using M_XRD and M_XWR */
+  val io = IO(new Bundle {})
+}
+
+/** Module connecting VCode accelerator directly to the L1-L2 crossbar connecting
+  * all tiles to other components in the system.
+  * @param p Implicit parameter passed by build system of top-level design parameters.
+  */
+class DMemFetcher(implicit p: Parameters) extends CoreModule()(p) {
+  val io = IO(new Bundle {})
+}
