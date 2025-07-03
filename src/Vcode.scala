f09fc31a07a46457c27a489f1cd5f1259936cef4
Xuyi Zhou <zhouxuyi20060401@gmail.com>
2025-07-03 16:16:24 -0500

    Updating Vcode.scala

--- src/Vcode.scala
+++ src/Vcode.scala
@@ -30,8 +30,12 @@ class VCodeAccel(opcodes: OpcodeSet)(implicit p: Parameters) extends LazyRoCC(op
   * custom opcode sets.
   */
 class VCodeAccelImp(outer: VCodeAccel) extends LazyRoCCModuleImp(outer) {
+  // io is "implicit" because we inherit from LazyRoCCModuleImp.
+  // io is the RoCCCoreIO
   val cmd = Queue(io.cmd)
-  cmd.ready := true.B
+  val rocc_cmd = cmd.bits // The entire RoCC Command provided to the accelerator
+  val rocc_inst = rocc_cmd.inst // The customX instruction in instruction stream
+  cmd.ready := true.B // Always ready to accept a command
 
   /* Create the decode table at the top-level of the implementation
    * If additional instructions are added as separate classes in Instructions.scala
@@ -39,8 +43,76 @@ class VCodeAccelImp(outer: VCodeAccel) extends LazyRoCCModuleImp(outer) {
   val decode_table = {
     Seq(new BinOpDecode)
   } flatMap(_.decode_table)
-  // Add the control signals
-  val ctrl_sigs = Reg(new CtrlSigs)
+
+  /***************
+   * DECODE
+   **************/
+  // Decode instruction, yielding control signals
+  val ctrl_sigs = Wire(new CtrlSigs()).decode(rocc_inst.funct, decode_table)
+
+  // If invalid instruction, raise exception
+  val exception = cmd.valid && !ctrl_sigs.legal
+  io.interrupt := exception
+  when(exception) {
+    if(p(VCodePrintfEnable)) {
+      printf("Raising exception to processor through interrupt!\nILLEGAL INSTRUCTION!\n");
+    }
+  }
+
+  /* The valid bit is raised to true by the main processor when the command is
+   * sent to the DecoupledIO Queue. */
+  when(cmd.valid) {
+    // TODO: Find a nice way to condense these conditional prints
+    if(p(VCodePrintfEnable)) {
+      printf("Got funct7 = 0x%x\trs1.val=0x%x\trs2.val=0x%x\n",
+        rocc_inst.funct, rocc_cmd.rs1, rocc_cmd.rs2)
+      printf("The instruction legal: %d\n", ctrl_sigs.legal)
+    }
+  }
+
+  /***************
+   * DATA FETCH
+   * Most instructions pass pointers to vectors, so we need to fetch that before
+   * operating on the data.
+   **************/
+  val data_fetcher = Module(new DCacheFetcher)
+
+  /***************
+   * EXECUTE
+   **************/
+  val alu = Module(new ALU)
+  val alu_out = Wire(UInt())
+  val alu_cout = Wire(UInt())
+  // Hook up the ALU to VCode signals
+  alu.io.fn := ctrl_sigs.alu_fn
+  // FIXME: Only use rs1/rs2 if xs1/xs2 =1, respectively.
+  alu.io.in1 := rocc_cmd.rs1
+  alu.io.in2 := rocc_cmd.rs2
+  alu_out := alu.io.out
+  alu_cout := alu.io.cout
+
+  /***************
+   * RESPOND
+   **************/
+  // Check if the accelerator needs to respond
+  val response_required = cmd.valid && ctrl_sigs.legal && rocc_inst.xd
+  val response = Reg(new RoCCResponse)
+  response.rd := rocc_inst.rd
+  response.data := alu_out
+  // Send response to main processor
+  /* TODO: Response can only be sent once all memory transactions and arithmetic
+   * operations have completed. */
+  when(response_required && io.resp.ready) {
+    if(p(VCodePrintfEnable)) {
+      printf("Main processor ready for response? %d\n", io.resp.ready)
+    }
+    io.resp.enq(response) // Sends response & sets valid bit
+    if(p(VCodePrintfEnable)) {
+      printf("VCode accelerator made response bits valid? %d\n", io.resp.valid)
+    }
+  }
+  // TODO: Find way to make valid response false when no response needed or ready
+  // io.resp.valid := false.B // Always invalid response until otherwise
 }
 
 /** Mixin to build a chip that includes a VCode accelerator.
@@ -51,4 +123,16 @@ class WithVCodeAccel extends Config((site, here, up) => {
       val vcodeAccel = LazyModule(new VCodeAccel(OpcodeSet.custom0)(p))
       vcodeAccel
     })
+})
+
+/** Design-level configuration option to toggle the synthesis of print statements
+  * in the synthesized hardware design.
+  */
+case object VCodePrintfEnable extends Field[Boolean](false)
+
+/** Mixin to enable print statements from the synthesized design.
+  * This mixin should only be used AFTER the WithVCodeAccel mixin.
+  */
+class WithVCodePrintf extends Config((site, here, up) => {
+  case VCodePrintfEnable => true
 })
